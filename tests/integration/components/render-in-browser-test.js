74fff3111064e894409f8d2d55de45521e24e6c2
Sivasubramanyam A <ssdragonar5@gmail.com>
2018-05-31 19:19:29 +0530

    Update ember-cli to v3

--- tests/integration/components/render-in-browser-test.js
+++ tests/integration/components/render-in-browser-test.js
@@ -1,86 +1,88 @@
-import { moduleForComponent, test } from 'ember-qunit';
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render, find } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('display-in-browser', 'Integration | Component | display in browser', {
-  integration: true
-});
+module('Integration | Component | display in browser', function(hooks) {
+  setupRenderingTest(hooks);
 
-test('it renders when all is specified', function(assert) {
-  this.render(hbs`
-    {{#display-in-browser all="true"}}
-      template block text
-    {{/display-in-browser}}
-  `);
+  test('it renders when all is specified', async function(assert) {
+    await render(hbs`
+      {{#display-in-browser all="true"}}
+        template block text
+      {{/display-in-browser}}
+    `);
 
-  assert.equal(this.$().text().trim(), 'template block text');
-});
+    assert.equal(find('*').textContent.trim(), 'template block text');
+  });
 
-test('it does not render when none is specified', function(assert) {
-  this.render(hbs`
-    {{#display-in-browser none="true"}}
-      template block text
-    {{/display-in-browser}}
-  `);
+  test('it does not render when none is specified', async function(assert) {
+    await render(hbs`
+      {{#display-in-browser none="true"}}
+        template block text
+      {{/display-in-browser}}
+    `);
 
-  assert.equal(this.$().text().trim(), '');
-});
+    assert.equal(find('*').textContent.trim(), '');
+  });
 
-test('it renders when only chrome is specified', function(assert) {
-  window.chrome = { why: 'because this wont be there in headless chrome' };
-  window.chrome.webstore = true;
+  test('it renders in Chrome when only chrome is specified', async function(assert) {
+    window.chrome = { why: 'because this wont be there in headless chrome' };
+    window.chrome.webstore = true;
 
-  this.render(hbs`
-    {{#display-in-browser only="chrome"}}
-      template block text
-    {{/display-in-browser}}
-  `);
+    await render(hbs`
+      {{#display-in-browser only="chrome"}}
+        template block text
+      {{/display-in-browser}}
+    `);
 
-  assert.equal(this.$().text().trim(), 'template block text');
+    assert.equal(find('*').textContent.trim(), 'template block text');
 
-  this.render(hbs`
-    {{#display-in-browser only="Chrome"}}
-      template block text
-    {{/display-in-browser}}
-  `);
+    await render(hbs`
+      {{#display-in-browser only="Chrome"}}
+        template block text
+      {{/display-in-browser}}
+    `);
 
-  assert.equal(this.$().text().trim(), 'template block text');
+    assert.equal(find('*').textContent.trim(), 'template block text');
 
-  this.set('only', ['Chrome']);
-  this.render(hbs`
-    {{#display-in-browser only=only}}
-      template block text
-    {{/display-in-browser}}
-  `);
+    this.set('only', ['Chrome']);
+    await render(hbs`
+      {{#display-in-browser only=only}}
+        template block text
+      {{/display-in-browser}}
+    `);
 
-  assert.equal(this.$().text().trim(), 'template block text');
-});
+    assert.equal(find('*').textContent.trim(), 'template block text');
+  });
 
-test('it does not render when except chrome is specified', function(assert) {
-  window.chrome = { why: 'because this wont be there in headless chrome' };
-  window.chrome.webstore = false;
+  test('it does not render in Chrome when except chrome is specified', async function(assert) {
+    window.chrome = { why: 'because this wont be there in headless chrome' };
+    window.chrome.webstore = true;
 
-  this.render(hbs`
-    {{#display-in-browser except="chrome"}}
-      template block text
-    {{/display-in-browser}}
-  `);
+    await render(hbs`
+      {{#display-in-browser except="chrome"}}
+        template block text
+      {{/display-in-browser}}
+    `);
 
-  assert.equal(this.$().text().trim(), 'template block text');
+    assert.equal(find('*').textContent.trim(), '');
 
-  this.render(hbs`
-    {{#display-in-browser except="Chrome"}}
-      template block text
-    {{/display-in-browser}}
-  `);
+    await render(hbs`
+      {{#display-in-browser except="Chrome"}}
+        template block text
+      {{/display-in-browser}}
+    `);
 
-  assert.equal(this.$().text().trim(), 'template block text');
+    assert.equal(find('*').textContent.trim(), '');
 
-  this.set('except', ['Chrome']);
-  this.render(hbs`
-    {{#display-in-browser except=except}}
-      template block text
-    {{/display-in-browser}}
-  `);
+    this.set('except', ['Chrome']);
+    await render(hbs`
+      {{#display-in-browser except=except}}
+        template block text
+      {{/display-in-browser}}
+    `);
 
-  assert.equal(this.$().text().trim(), 'template block text');
+    assert.equal(find('*').textContent.trim(), '');
+  });
 });
